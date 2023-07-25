5cd5b3e3cee0185e1d82f4979b5c569eba2a6dac
Peak <pglund18@yahoo.com>
2023-07-24 23:05:47 -0500

    Main works again

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/api/notes', (req, res) => {
 
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('*', (req, res) => {
